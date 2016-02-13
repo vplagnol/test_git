891bb7b9a97c54d11fc66f7d6619cb0be4224e76
Vincent Plagnol <vincent.plagnol@inivata.com>
2016-02-13 10:22:05 +0000

    See changelogs

--- commit.sh
+++ commit.sh
@@ -1,5 +1,10 @@
+branch=$1
+
+#git rev-parse --verify <branch_name>
+
+if [ "$branch" == "" ]; then echo "Need to specify a branch name"; exit; fi
+
 git add -u
 git commit -m "See changelogs"
-#git push -u origin master
-git push -u origin devel
+git push -u origin $branch
 
